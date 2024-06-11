3cca555d803c1fbb1540d9dc05d3c60765541b3f
Mohd Shadab <shadab_ibrahim@outlook.com>
2024-06-11 11:33:23 +0530

    submitting code-Tue Jun 11 2024 11:33:22 GMT+0530 (India Standard Time)

---
+++ src/components/Description.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Description = () => {
+    return (
+        <h3 data-ns-test="project-description">Project Description</h3>
+    )
+}
+export default Description;
---
+++ src/components/Name.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Name = () => {
+    return (
+        <h1 data-ns-test="project-name">Name of the Project </h1>
+    )
+}
+export default Name;
